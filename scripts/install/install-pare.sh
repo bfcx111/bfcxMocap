40406050d5853d3770b1c924f77e4dddffdfcbea
bfcx111 <774682978@qq.com>
2022-07-04 22:28:59 +0800

    update scripts/install

---
+++ scripts/install/install-pare.sh
@@ -0,0 +1,36 @@
+
+pip install opencv-python
+pip install joblib
+pip install tqdm
+pip install -U albumentations
+
+pip install yacs
+pip install numba
+
+
+pip install filterpy
+pip install flatten-dict
+pip install fastai
+pip install jpeg4py
+pip install chumpy
+pip install torchgeometry
+pip install pytorch-lightning
+pip install loguru
+pip install smplx
+
+
+# mkdir -p 3rdparty
+# cd 3rdparty
+# git clone https://github.com/mmatl/pyopengl.git
+# python3 -m pip uninstall pyopengl
+# python3 -m pip install ./pyopengl
+# echo "export PYOPENGL_PLATFORM=osmesa" >> ~/.zshrc
+# export PYOPENGL_PLATFORM=osmesa
+pip install pyopengl
+pip install trimesh
+pip install pyrender
+
+pip install git+https://github.com/mkocabas/yolov3-pytorch.git
+pip install git+https://github.com/mkocabas/yolov3-pytorch.git
+pip install git+https://github.com/mkocabas/multi-person-tracker.git
+pip install git+https://github.com/giacaglia/pytube.git
